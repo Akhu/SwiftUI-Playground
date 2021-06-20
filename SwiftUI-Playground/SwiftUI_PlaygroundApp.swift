2aaf9b92da349e07a322e105f8d8ad674b44e085
Anthony <me@anthony-dacruz.com>
2021-06-20 14:32:32 +0200

    Initial Commit

---
+++ SwiftUI-Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI-Playground
+//
+//  Created by Anthony Da cruz on 20/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
